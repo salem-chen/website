9709ec3b8650ba45f60b42f89b3d61348796651e
salem-chen <910802419@qq.com>
2019-07-22 02:23:05 +0800

    update

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,13 +1,37 @@
 <?php
 namespace app\admin\controller;
 
+use think\Db;
 
-use think\Controller;
-
-class Index
+class Index extends Base
 {
-    public function index()
+    public function index($pid = 0,$id = 0)
     {
-        return view("index");
+        //导航信息
+        $data['navigator'] = Db::table("web_navigator")
+                      ->field("id,name,display_name")
+                      ->where([
+                          "is_show"   => 1,
+                          "parent_id" => 0,
+                          "is_delete" => 0
+                      ])
+                      ->order('sort','desc')
+                      ->select();
+        $data['leftNavigator'] = [];
+        if($pid){
+            $data['leftNavigator'] = Db::table("web_navigator")
+                ->field("id,name,display_name")
+                ->where([
+                    "is_show"   => 1,
+                    "parent_id" => $pid,
+                    "is_delete" => 0
+                ])
+                ->order('sort','desc')
+                ->select();
+        }
+
+        $data['pid'] = $pid;
+        $data['id']  = $id;
+        return view("index",$data);
     }
 }
